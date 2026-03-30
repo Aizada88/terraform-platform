39f8c25882759d3ae38a5a0926a6e0d8fc8b78a0
Aizada <-email@gmail.com>
2026-03-30 10:57:09 -0500

    fix correct s3 backend

--- envs/prod/versions.tf
+++ envs/prod/versions.tf
@@ -2,15 +2,8 @@ terraform {
   required_version = ">= 1.6.0"
 
   backend "s3" {
-    bucket = "aizada-terraform-state-12345"
+    bucket = "aiza-terraform-state-bucket26"
     key    = "prod/terraform.tfstate"
-    region = "us-east-1"
-  }
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
+    region = "us-east-2"
   }
 }
