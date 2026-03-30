586ac6fbdec037694a542cf2863ea7a191b38b26
Aizada <-email@gmail.com>
2026-03-30 10:51:50 -0500

    add backend config

--- envs/prod/versions.tf
+++ envs/prod/versions.tf
@@ -1,7 +1,11 @@
 terraform {
   required_version = ">= 1.6.0"
 
-  backend "s3" {}
+  backend "s3" {
+    bucket = "aizada-terraform-state-12345"
+    key    = "prod/terraform.tfstate"
+    region = "us-east-1"
+  }
 
   required_providers {
     aws = {
